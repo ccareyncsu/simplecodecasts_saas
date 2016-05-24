46186424c736a78749464472da26e287796a64f8
Chasen Arey <ncsuarey@hotmail.com>
2016-05-24 13:21:29 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523175138) do
+ActiveRecord::Schema.define(version: 20160524131215) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
