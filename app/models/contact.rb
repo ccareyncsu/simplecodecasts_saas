9365a2c26ae04e398f6f16da5e7a5a6e6b9c3d31
Chasen Arey <ncsuarey@hotmail.com>
2016-12-29 05:01:13 +0000

    fixed on master

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,5 @@
 class Contact < ActiveRecord::Base
     validates :name, presence: true
     validates :email, presence: true
-<<<<<<< HEAD
 end
 
-<<<<<<< HEAD
-
-=======
- 
-=======
->>>>>>> stripe_integration
-end
->>>>>>> development
